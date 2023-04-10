4cb376db180d20ea7d12dac08ead0bbddb4438d4
Ryan Feroz <56842734+ryanferoz@users.noreply.github.com>
2023-04-10 19:24:11 -0400

    Add Recycler Foundation being Recipe, Adapter, and Fetcher

---
+++ app/src/main/java/com/example/munch/Recipe.kt
@@ -0,0 +1,10 @@
+package com.example.munch
+
+import android.media.Image
+
+class Recipe(
+    val ingredients: String,
+    val title: String,
+    val photo: String
+) {
+}
---
+++ app/src/main/java/com/example/munch/RecipeFetcher.kt
@@ -0,0 +1,26 @@
+package com.example.munch
+
+class RecipeFetcher {
+    companion object {
+        val ingredients = listOf("Dahlia Cline", "Kevin Miranda", "Kaya Austin", "Laila Calderon", "Marquise Rhodes", "Fletcher Patel", "Luz Barron", "Kamren Dudley", "Jairo Foster", "Lilah Sandoval", "Ansley Blake", "Slade Sawyer", "Jaelyn Holmes", "Phoenix Bright", "Ernesto Gould")
+        val title = "Pork Fried Rice"
+        val photo = "Welcome to the Android Kotlin Course! We're excited to have you join us and learn how to develop Android apps using Kotlin. Here are some tips to get started."
+        fun getRecipe(): MutableList<Recipe> {
+            var recipes : MutableList<Recipe> = ArrayList()
+            for (i in 0..9) {
+                val recipe = Recipe(ingredients[i], title, photo)
+                recipes.add(recipe)
+            }
+            return recipes
+        }
+
+        fun getNext5Recipes(): MutableList<Recipe> {
+            var newRecipes: MutableList<Recipe> = ArrayList()
+            for (i in 10..14) {
+                val recipe = Recipe(ingredients[i], title, photo)
+                newRecipes.add(recipe)
+            }
+            return newRecipes
+        }
+    }
+}
