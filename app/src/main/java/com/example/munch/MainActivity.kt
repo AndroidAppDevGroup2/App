28e1ae39915b4dd21d244445e9f7df38fc090e09
Gianni Gabriel <gianni@momento.fm>
2023-04-17 00:51:19 -0400

    Add bottom navbar

--- app/src/main/java/com/example/munch/MainActivity.kt
+++ app/src/main/java/com/example/munch/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.munch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
     }
 }
