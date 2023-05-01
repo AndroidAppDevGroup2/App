672c1efb8dda581d55a517d0cc0b974262bf5b06
angelotvelardi <avelardi9@gmail.com>
2023-05-01 16:43:08 -0400

    Write Saved Recipe to database

--- app/src/main/java/com/example/munch/DetailActivity.kt
+++ app/src/main/java/com/example/munch/DetailActivity.kt
@@ -1,22 +1,22 @@
 package com.example.munch
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-
-
-
-private const val TAG = "DetailActivity"
 class DetailActivity : AppCompatActivity() {
     private lateinit var recipeImageView: ImageView
     private lateinit var titleTextView: TextView
     private lateinit var instructionsTextView: TextView
     private lateinit var ingredientsTextView: TextView
-
+    private lateinit var saveButton: Button
+    private lateinit var  dbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,24 +25,32 @@ class DetailActivity : AppCompatActivity() {
 
         recipeImageView = findViewById(R.id.recipeImage)
         titleTextView = findViewById(R.id.title)
-        instructionsTextView = findViewById(R.id.instructions)
-        ingredientsTextView = findViewById(R.id.ingredients)
-
+        instructionsTextView = findViewById(R.id.ingredients)
+        ingredientsTextView = findViewById(R.id.instructions)
+        saveButton = findViewById(R.id.button)
+        dbRef = FirebaseDatabase.getInstance().getReference("Recipes")
         val recipe = intent.getSerializableExtra(RECIPE_EXTRA) as Recipe
+
         titleTextView.text = recipe.title
-
-        var LOI=recipe.ingredients.joinToString(separator = "\n")+"\n"+ recipe.ingredients2.joinToString(separator = "\n")
-        LOI=LOI.replace("Ingredient(name=","")
-        LOI=LOI.replace(")","")
-        LOI=LOI.replace("(","")
-        println(LOI)
-        ingredientsTextView.text = LOI
-
+        ingredientsTextView.text = recipe.ingredients.toString()
         //ingredientsTextView.text = recipe.abstract
 
         Glide.with(this)
             .load(recipe.image)
-            .apply(RequestOptions().override(1200, 400))
             .into(recipeImageView)
+
+        saveButton.setOnClickListener {
+            saveRecipeData(recipe)
+        }
+    }
+
+    private fun saveRecipeData(recipe: Recipe){
+        val recipeId = dbRef.push().key!!
+        dbRef.child(recipeId).setValue(recipe)
+            .addOnCompleteListener {
+                Toast.makeText(this, "Recipe Saved Successfully", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener { err ->
+                Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
