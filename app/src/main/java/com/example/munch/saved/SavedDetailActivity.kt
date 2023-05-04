276ad028badc9db82398598271ffd3117dfac359
Gianni Gabriel <gianni@momento.fm>
2023-05-03 21:20:33 -0400

    Add instructions to saved details screen

--- app/src/main/java/com/example/munch/saved/SavedDetailActivity.kt
+++ app/src/main/java/com/example/munch/saved/SavedDetailActivity.kt
@@ -5,11 +5,21 @@ import com.example.munch.RECIPE_EXTRA
 import com.example.munch.Recipe
 import com.example.munch.Steps
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.example.munch.BuildConfig
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.Headers
+import org.json.JSONArray
+
+private const val SEARCH_API_KEY = BuildConfig.API_KEY
 
 class SavedDetailActivity : AppCompatActivity() {
     private lateinit var recipeImageView: ImageView
@@ -17,6 +27,8 @@ class SavedDetailActivity : AppCompatActivity() {
     private lateinit var instructionsTextView: TextView
     private lateinit var ingredientsTextView: TextView
     private lateinit var instructionsList: List<Steps>
+
+    val client = AsyncHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +41,7 @@ class SavedDetailActivity : AppCompatActivity() {
         ingredientsTextView = findViewById(R.id.ingredients)
 
         val recipe = intent.getSerializableExtra(RECIPE_EXTRA) as Recipe
+        val id=recipe.id
         titleTextView.text = recipe.title
 
         var LOI=recipe.ingredients.joinToString(separator = "\n")+"\n"+ recipe.ingredients2.joinToString(separator = "\n")
@@ -37,6 +50,63 @@ class SavedDetailActivity : AppCompatActivity() {
         LOI=LOI.replace("(","")
 
         ingredientsTextView.text = LOI
+
+        client[
+                "https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=${SEARCH_API_KEY}",
+                object : JsonHttpResponseHandler()
+                {
+                    /*
+                     * The onSuccess function gets called when
+                     * HTTP response status is "200 OK"
+                     */
+                    override fun onSuccess(
+                        statusCode: Int,
+                        headers: Headers,
+                        json: JSON
+                    ) {
+                        // The wait for a response is over
+
+
+                        val resultsJSON : JSONArray = json.jsonArray
+                        Log.v("JSONArray", resultsJSON.toString())
+                        val recipesRawJSON : String = resultsJSON.toString()
+                        val gson = Gson()
+                        val arrayTutorialType = object : TypeToken<List<Steps>>() {}.type
+                        val models : List<Steps> = gson.fromJson(recipesRawJSON, arrayTutorialType)
+                        instructionsList = models
+                        if (models.isEmpty()){
+                            instructionsTextView.text="no instructions found :("
+                        }
+                        else {
+                            var instructions=models.joinToString(separator = "\n")
+                            instructions=instructions.replace("Steps(instructions=[Instructions(number=","")
+                            instructions=instructions.replace("Instructions(number=","\n")
+                            instructions=instructions.replace("step=","")
+                            instructions=instructions.replace(")","")
+                            instructions=instructions.replace("]","")
+                            instructionsTextView.text = instructions
+                        }
+                    }
+
+                    /*
+                     * The onFailure function gets called when
+                     * HTTP response status is "4XX" (eg. 401, 403, 404)
+                     */
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Headers?,
+                        errorResponse: String,
+                        t: Throwable?
+                    ) {
+                        // The wait for a response is over
+
+
+                        // If the error is not null, log it!
+                        t?.message?.let {
+                            Log.e("BestSellerBooksFragment", errorResponse)
+                        }
+                    }
+                }]
 
         Glide.with(this)
             .load(recipe.image)
